2ba81febaa71b0341a6c628f0e125619e1d3e414
Fred <delon.frederic.pro.job@gmail.com>
2016-12-20 13:35:42 +0100

    step 2 end

--- src/app.es6.js
+++ src/app.es6.js
@@ -1,15 +1,23 @@
 let nameTab = [];
 
-let sayMyName = () => {
-	let lol = document.getElementById('name').value;
-	if(lol === ""){
-		document.getElementById("hello").innerHTML = "salut le pere Z'oreils !";
-	}
-	else{
-		document.getElementById("hello").innerHTML = `Salut ${lol}`;
-	let tab =	document.body.appendChild(document.createElement('li')).innerHTML = lol;      
-	 nameTab.push(lol);
-	console.log(nameTab)
 
+document.getElementById('btn').addEventListener('click', () => {
+
+	let lol = document.getElementById('name').value;
+	lol = lol.charAt(0).toUpperCase() + lol.substr(1).toLowerCase();
+
+	if(lol === "") {
+
+		document.getElementById("hello").innerHTML = "salut le pere Z'oreils !";
+	
+	} else {
+
+		nameTab.push(lol);
+		document.getElementById("hello").innerHTML = `Salut ${lol}`;
+		let tab =	document.body.appendChild(document.createElement('li')).innerHTML = lol;      
+		console.log(nameTab)
 	}
-}
+});
+
+
+
